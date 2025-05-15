7534b401dba77cd974c3d2b52985e41d40f9d7c8
Andrei Istoc <andreistoc@gmail.com>
2025-05-15 14:45:44 +0300

    Update Gemini.swift

--- Sources/LangChain/llms/Gemini.swift
+++ Sources/LangChain/llms/Gemini.swift
@@ -4,16 +4,24 @@
 //
 //  Created by 顾艳华 on 12/25/23.
 //
-
 import Foundation
 import GoogleGenerativeAI
 
 public class Gemini: LLM {
+    let modelName: String // Add this property
+
+    // Modify the initializer to accept a model name
+    public init(modelName: String = "gemini-pro", callbacks: [BaseCallbackHandler] = [], cache: BaseCache? = nil) {
+        self.modelName = modelName
+        super.init(callbacks: callbacks, cache: cache)
+    }
+
     override func _send(text: String, stops: [String]) async throws -> LLMResult {
         let env = LC.loadEnv()
         
         if let apiKey = env["GOOGLEAI_API_KEY"] {
-            let model = GenerativeModel(name: "gemini-pro", apiKey: apiKey)
+            // Use the modelName property here
+            let model = GenerativeModel(name: self.modelName, apiKey: apiKey)
             let response = try await model.generateContent(text)
             return LLMResult(llm_output: response.text)
         } else {
